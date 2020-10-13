5161043c38aec93d93b471ae87b2c4786252f51f
Michelle Huang <yunn788@gmail.com>
2020-10-13 00:58:22 +0000

    Class form interaction activity

---
+++ script.js
@@ -0,0 +1,42 @@
+// P5 JAVASCRIPT FUNCTIONS
+///////////////////////////////////////////////////////////////////
+
+// Insert your setup and draw functions here
+function setup(){
+  // Creates a canvas with the same dimensions as the browser window
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
+  // Attaches the canvas to the body element with the ID 'canvas-container'
+  canvas.parent('canvas-container');
+}
+
+function draw(){
+  // Sets the canvas background every frame
+  
+  fill(247, 247, 247);
+  strokeWeight(2)
+  ellipse(mouseX,mouseY,10);
+  
+}
+
+// REGULAR JAVASCRIPT FUNCTIONS
+///////////////////////////////////////////////////////////////////
+
+// Sets the form element as a variable for easy reference
+var form = document.querySelector("form");
+// Adds the event listener for when the form is submitted
+form.addEventListener("submit", (event) => {
+  // Stops the form from trying to load another page
+  event.preventDefault();
+  // Creates an array from all the elements inside the form element and loops through the array
+  Array.from(form.elements).forEach((element) => {
+    // Finds any input which is not the submit button
+    if (element.type != "submit") {
+      // Logs the 'name' attribute from the HTML element
+      console.log(element.name + ':');
+      // Logs the 'value' attribute from the HTML element
+      console.log(element.value);
+    }
+  }); 
+});
+
+
